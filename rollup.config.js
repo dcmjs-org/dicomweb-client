ceb392ac481b3d061ee70bd1f54e99ff6398391f
hackermd <markusdherrmann@gmail.com>
2018-07-29 18:16:50 -0400

    Update package and rollup files

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,26 @@
+import builtins from 'rollup-plugin-node-builtins';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+
 export default {
-    entry: 'src/dicomweb-client.js',
-    targets: [
-        {
-            dest: 'build/dicomweb-client.js',
-            format: 'umd',
-            moduleName: 'DICOMwebClient',
-        sourceMap: true
-        },
-    ]
+  entry: 'src/dicomweb-client.js',
+  targets: [
+    {
+      dest: 'build/dicomweb-client.js',
+      format: 'umd',
+      name: 'DICOMwebClient',
+      sourceMap: true
+    },
+  ],
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    builtins(),
+    json()
+  ]
 };
