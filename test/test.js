f7f2d447fad9233817fc5bb35c1dc17578b9685d
Erik Ziegler <erik.sweed@gmail.com>
2018-09-20 18:09:32 +0200

    chore(tests): Remove duplicated StoreInstances test

--- test/test.js
+++ test/test.js
@@ -38,32 +38,6 @@ describe('dicomweb.api.DICOMwebClient', function () {
     // This is the HTTP server run by the Karma test
     // runner
     const url = 'http://localhost:9876/base/testData/sample.dcm';
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "arraybuffer";
-
-    xhr.onload = function (oEvent) {
-      const arrayBuffer = this.response;
-      if (arrayBuffer) {
-        const options = {
-          datasets: [arrayBuffer]
-        };
-
-        dwc.storeInstances(options).then(function() {
-          done();
-        }, done);
-      }
-    };
-
-    xhr.send();
-  });
-
-  it('should store one instance', function(done) {
-    this.timeout(5000);
-
-    // This is the HTTP server run by the Karma test
-    // runner
-    const url = 'http://localhost:9876/base/testData/sample.dcm';
 
     getTestDataInstance(url).then(arrayBuffer => {
       const options = {
