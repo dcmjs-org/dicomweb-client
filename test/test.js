7cc36bad512dd3eb6177b19a9930330a13f98549
Erik Ziegler <erik.sweed@gmail.com>
2018-09-20 12:14:32 +0200

    feat(stow-rs): Add support for STOW-RS StoreInstances with more than one instance (#7)

--- test/test.js
+++ test/test.js
@@ -1,3 +1,22 @@
+function getTestDataInstance(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "arraybuffer";
+
+    xhr.onload = function() {
+      const arrayBuffer = this.response;
+      if (arrayBuffer) {
+        resolve(arrayBuffer);
+      } else {
+        reject(new Error('Something went wrong...'));
+      }
+    };
+
+    xhr.send();
+  });
+}
+
 describe('dicomweb.api.DICOMwebClient', function () {
   const dwc = new DICOMwebClient.api.DICOMwebClient({
     url: 'http://localhost:8008/dcm4chee-arc/aets/DCM4CHEE/rs',
@@ -39,9 +58,57 @@ describe('dicomweb.api.DICOMwebClient', function () {
     xhr.send();
   });
 
+  it('should store one instance', function(done) {
+    this.timeout(5000);
+
+    // This is the HTTP server run by the Karma test
+    // runner
+    const url = 'http://localhost:9876/base/testData/sample.dcm';
+
+    getTestDataInstance(url).then(arrayBuffer => {
+      const options = {
+        datasets: [arrayBuffer]
+      };
+
+      dwc.storeInstances(options).then(function() {
+        done();
+      }, done);
+    });
+  });
+
   it('should find one study', function() {
     return dwc.searchForStudies().then(studies => {
       chai.expect(studies).to.have.length(1);
+    });
+  });
+
+  it('should store two instances', function(done) {
+    this.timeout(10000);
+
+    // This is the HTTP server run by the Karma test
+    // runner
+    const url1 = 'http://localhost:9876/base/testData/sample2.dcm';
+    const url2 = 'http://localhost:9876/base/testData/sample3.dcm';
+
+    const instancePromises = [
+      getTestDataInstance(url1),
+      getTestDataInstance(url2),
+    ];
+
+    Promise.all(instancePromises).then(datasets => {
+      const options = {
+        datasets
+      };
+
+      dwc.storeInstances(options).then(function() {
+        done();
+      }, done);
+    });
+  });
+
+  it('should find three studes', function() {
+    return dwc.searchForStudies().then(studies => {
+      chai.expect(studies).to.have.length(3);
     });
   });
 });
