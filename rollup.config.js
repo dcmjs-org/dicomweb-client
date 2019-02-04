83e37ea459062e785c304a9409a72fa0c684375c
Erik Ziegler <erik.sweed@gmail.com>
2019-02-04 15:58:11 +0100

    feat(es-module): Add ES module output to Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,21 @@
 import babel from 'rollup-plugin-babel';
+import pkg from './package.json'
 
 export default {
   input: 'src/dicomweb-client.js',
-  output: {
-      file: 'build/dicomweb-client.js',
+  output: [{
+      file: pkg.main,
       format: 'umd',
       name: 'DICOMwebClient',
       sourcemap: true,
-  }, 
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+      exports: 'named'
+    }
+  ], 
   plugins: [
 	babel({
 	  runtimeHelpers: true,
