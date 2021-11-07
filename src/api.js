87b2da9faa7caa3c0d4cfbbc205e3ebde9119d98
hackermd <markusdherrmann@gmail.com>
2021-11-06 20:40:54 -0400

    Allow accept header field with mixed media types
    This enables request of compressed or uncompressed frames

--- src/api.js
+++ src/api.js
@@ -885,41 +885,53 @@ class DICOMwebClient {
   }
 
   /**
+   * Gets types that are shared among acceptable media types.
+   *
+   * @param {Object[]} mediaTypes - Acceptable media types and optionally the UIDs of the
+   corresponding transfer syntaxes
+   * @private
+   * @returns {String[]} Types that are shared among acceptable media types
+   */
+  static _getSharedMediaTypes(mediaTypes) {
+    const types = new Set();
+
+    if (!mediaTypes || !mediaTypes.length) {
+      return types
+    }
+
+    mediaTypes.forEach(item => {
+      const { mediaType } = item;
+      const type = DICOMwebClient._parseMediaType(mediaType)[0];
+      types.add(`${type}/`);
+    });
+
+    return Array.from(types)
+  }
+
+  /**
    * Gets common type of acceptable media types and asserts that only
    one type is specified. For example, ``("image/jpeg", "image/jp2")``
    will pass, but ``("image/jpeg", "video/mpeg2")`` will raise an
    exception.
    *
-   * @param {String[]} mediaTypes - Acceptable media types and optionally the UIDs of the
+   * @param {Object[]} mediaTypes - Acceptable media types and optionally the UIDs of the
    corresponding transfer syntaxes
    * @private
-   * @returns {Array} Common media type
+   * @returns {String[]} Common media type
    */
   static _getCommonMediaType(mediaTypes) {
     if (!mediaTypes || !mediaTypes.length) {
       throw new Error("No acceptable media types provided");
     }
 
-    const commonMediaTypes = new Set();
-    mediaTypes.forEach(item => {
-      const { mediaType } = item;
-
-      if (mediaType.startsWith("application")) {
-        commonMediaTypes.add(mediaType);
-      } else {
-        const type = DICOMwebClient._parseMediaType(mediaType)[0];
-
-        commonMediaTypes.add(`${type}/`);
-      }
-    });
-
-    if (commonMediaTypes.size === 0) {
+    const sharedMediaTypes = DICOMwebClient._getSharedMediaTypes(mediaTypes);
+    if (sharedMediaTypes.length === 0) {
       throw new Error("No common acceptable media type could be identified.");
-    } else if (commonMediaTypes.size > 1) {
+    } else if (sharedMediaTypes.length > 1) {
       throw new Error("Acceptable media types must have the same type.");
     }
 
-    return Array.from(commonMediaTypes)[0];
+    return sharedMediaTypes[0];
   }
 
   /**
@@ -1190,24 +1202,65 @@ class DICOMwebClient {
         withCredentials = options.withCredentials;
       }
     }
-    
+
     let progressCallback = false;
     if ("progressCallback" in options) {
       progressCallback = options.progressCallback;
     }
 
     if (!mediaTypes) {
-      return this._httpGetMultipartApplicationOctetStream(url, false, false, false, progressCallback, withCredentials);
+      return this._httpGetMultipartApplicationOctetStream(
+        url, false, false, false, progressCallback, withCredentials
+      );
+    }
+
+    const sharedMediaTypes = DICOMwebClient._getSharedMediaTypes(mediaTypes);
+    if (sharedMediaTypes.length > 1) {
+      /**
+       * Enable request of frames that are stored either compressed
+       * (image/* media type) or uncompressed (application/octet-stream
+       * media type).
+       */
+      const supportedMediaTypes = {
+        "1.2.840.10008.1.2.1": ["application/octet-stream"],
+        "1.2.840.10008.1.2.5": ["image/x-dicom-rle"],
+        "1.2.840.10008.1.2.4.50": ["image/jpeg"],
+        "1.2.840.10008.1.2.4.51": ["image/jpeg"],
+        "1.2.840.10008.1.2.4.57": ["image/jpeg"],
+        "1.2.840.10008.1.2.4.70": ["image/jpeg"],
+        "1.2.840.10008.1.2.4.80": ["image/x-jls", "image/jls"],
+        "1.2.840.10008.1.2.4.81": ["image/x-jls", "image/jls"],
+        "1.2.840.10008.1.2.4.90": ["image/jp2"],
+        "1.2.840.10008.1.2.4.91": ["image/jp2"],
+        "1.2.840.10008.1.2.4.92": ["image/jpx"],
+        "1.2.840.10008.1.2.4.93": ["image/jpx"],
+      }
+
+      const headers = {
+        Accept: DICOMwebClient._buildMultipartAcceptHeaderFieldValue(
+          mediaTypes,
+          supportedMediaTypes
+        )
+      }
+      return this._httpGet(
+        url, headers, "arraybuffer", progressCallback, withCredentials
+      ).then(multipartDecode);
     }
 
     const commonMediaType = DICOMwebClient._getCommonMediaType(mediaTypes);
 
     if (commonMediaType === MEDIATYPES.OCTET_STREAM) {
-      return this._httpGetMultipartApplicationOctetStream(url, mediaTypes, false, false, progressCallback, withCredentials);
+      return this._httpGetMultipartApplicationOctetStream(
+        url, mediaTypes, false, false, progressCallback, withCredentials
+      );
     } else if (commonMediaType.startsWith("image")) {
-      return this._httpGetMultipartImage(url, mediaTypes, false, false, false, progressCallback, withCredentials);
+      return this._httpGetMultipartImage(
+        url, mediaTypes, false, false, false, progressCallback, withCredentials
+      );
     } else if (commonMediaType.startsWith("video")) {
-      return this._httpGetMultipartVideo(url, mediaTypes, false, false, false, progressCallback, withCredentials);
+      return this._httpGetMultipartVideo(
+        url, mediaTypes, false, false, false, progressCallback, withCredentials
+      );
     }
 
     throw new Error(
