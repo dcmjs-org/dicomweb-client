39d9899e2d6f71d5ff04018da9ee311768b43dd0
hackermd <markusdherrmann@gmail.com>
2022-12-12 23:20:53 -0500

    Use standard instead of using eslint directly
    Also fix reported coding style issues

--- src/dicomweb-client.js
+++ src/dicomweb-client.js
@@ -1,21 +1,21 @@
-import { DICOMwebClient } from "./api.js";
+import { DICOMwebClient } from './api.js'
 import {
   getStudyInstanceUIDFromUri,
   getSeriesInstanceUIDFromUri,
   getSOPInstanceUIDFromUri,
   getFrameNumbersFromUri
-} from "./utils.js";
+} from './utils.js'
 
 const api = {
   DICOMwebClient
-};
+}
 const utils = {
   getStudyInstanceUIDFromUri,
   getSeriesInstanceUIDFromUri,
   getSOPInstanceUIDFromUri,
   getFrameNumbersFromUri
-};
+}
 
-export { default as version } from "./version.js";
+export { default as version } from './version.js'
 
-export { api, utils };
+export { api, utils }
